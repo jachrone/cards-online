7ba4d67b2bc504dc823fd992210281f57225c7f9
Zariman <zariman972@gmail.com>
2025-02-08 15:22:42 +0100

    hold player list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 use std::io::{self, Write};
+use crate::user::Player;
+
+mod user {
+    // A public struct with a public field of generic type `T`
+    #[derive(Debug)]
+    pub struct Player {
+        pub name: String,
+    }
+}
+
 
 fn main() {
     println!("Hello, welcome to card online");
@@ -12,4 +22,22 @@ fn main() {
     let player_count: i32 = player_count_s.trim().parse().expect("Please type a number!");
 
     println!("Hello, you will be playing with {} players!", player_count);
+
+    let mut players = Vec::new();
+    for i in 1..=player_count {
+        print!("Please enter the name of player n°{}: ", i);
+        io::stdout().flush().unwrap();
+
+        let mut player_name = String::new();
+        io::stdin().read_line(&mut player_name).expect("Failed to read line");
+
+        players.push(
+            Player 
+            { 
+                name: player_name.trim().to_string()
+            }
+        );
+
+    }
+    println!("Players: {:?}", players);
 }
