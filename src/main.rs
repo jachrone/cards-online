28f90800ebca91b59e52bf76ea1a8ae062485c6a
Zariman <zariman972@gmail.com>
2025-02-16 17:16:32 +0100

    handle river and test moving cards

--- src/main.rs
+++ src/main.rs
@@ -309,6 +309,7 @@ mod user {
     #[derive(Debug)]
     pub struct Player {
         pub name: String,
+        pub player_id: i32,
     }
 
     impl fmt::Display for Player {
@@ -319,36 +320,89 @@ mod user {
 }
 
 mod online_board {
+    use rand::{rng, seq::SliceRandom};
+
     use crate::{game, user::Player};
     use std::fmt;
 
     pub struct Seat {
         pub player: Player,
         pub hand: Vec<Box<dyn game::Card>>,
-        pub plis: Vec<Vec<Box<dyn game::Card>>>,
+        pub plis: Vec<Box<dyn game::Card>>,
     }
 
-    struct River {
-        //todo find holder of pair
-        //pub cards: Vec<Tuple<&Player, Box<dyn game::Card>>>,
-        pub requested_color: Option<game::CardColor>,
-        // todo
-        // fn play_card(&mut self, player: &Player, card: Box<dyn game::Card>) {
-        //     self.cards.push((player, card));
-        // }
+    pub struct PlayedCard {
+        order: i32,
+        player_id: i32,
+        card: Box<dyn game::Card>,
+    }
+
+    pub fn play_card(
+        table_river: &mut Vec<PlayedCard>,
+        player: &Player,
+        card: Box<dyn game::Card>,
+    ) {
+        let played_card = PlayedCard {
+            order: table_river.len() as i32,
+            player_id: player.player_id,
+            card,
+        };
+        table_river.push(played_card);
+    }
+
+    pub fn store_winner_fold(table: &mut Table, player_id: i32) {
+        let mut winner_cards = Vec::new();
+        while let Some(card) = table.river.pop() {
+            winner_cards.push(card.card);
+        }
+        if let Some(seat) = table
+            .seats
+            .iter_mut()
+            .find(|seat| seat.player.player_id == player_id)
+        {
+            seat.plis.extend(winner_cards);
+        }
+    }
+
+    pub fn clear_table_after_game(table: &mut Table) {
+        let mut all_cards = Vec::new();
+
+        // Retrieve cards from seated players
+        for seat in table.seats.iter_mut() {
+            all_cards.append(&mut seat.hand);
+            all_cards.append(&mut seat.plis);
+        }
+
+        // Retrieve cards from river if any are left
+        while let Some(played_card) = table.river.pop() {
+            all_cards.push(played_card.card);
+        }
+
+        // Put all cards back in the deck
+        table.deck.cards.append(&mut all_cards);
+
+        // Shuffle the deck
+        let mut rng = rng();
+        table.deck.cards.shuffle(&mut rng);
     }
 
     pub struct Table {
         pub seats: Vec<Seat>,
         pub deck: game::Deck,
-        //river: River,
+        pub river: Vec<PlayedCard>,
         seat_count: i32,
     }
 
     impl fmt::Display for Seat {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             write!(f, "{}", self.player);
-            write!(f, "\thand:\n");
+
+            write!(f, "\tplis: {} cards\n", self.plis.len());
+            for card in &self.plis {
+                write!(f, "\t\t{}", card);
+            }
+
+            write!(f, "\thand: {} cards\n", self.hand.len());
             // Print each card in the deck
             Ok(for card in &self.hand {
                 write!(f, "\t\t{}", card);
@@ -358,13 +412,17 @@ mod online_board {
 
     impl fmt::Display for Table {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "\tDeck:\n\t\tcards:\n");
+            write!(f, "\tDeck:\n\t\t{} cards:\n", self.deck.cards.len());
             for card in &self.deck.cards {
                 write!(f, "\t\t\t{}", card);
             }
             write!(f, "Seats:\n");
-            Ok(for seat in &self.seats {
+            for seat in &self.seats {
                 write!(f, "{}\n", seat);
+            }
+            write!(f, "River {} cards:\n", self.river.len());
+            Ok(for played_card in &self.river {
+                write!(f, "\t{}", played_card.card);
             })
         }
     }
@@ -374,7 +432,7 @@ mod online_board {
             seats: Vec::new(),
             deck: game::create_default_deck(),
             seat_count: player_count,
-            //river: Vec::new(),
+            river: Vec::new(),
         }
     }
 
@@ -418,15 +476,21 @@ fn main() {
 
         table.seats.push(new_seat(Player {
             name: player_name.trim().to_string(),
+            player_id: i,
         }));
     }
 
     table.deck = game::create_default_deck();
 
+    println!("Fresh Table:\n{}", table);
+
     // Shuffle the deck
     let mut rng = rng();
     table.deck.cards.shuffle(&mut rng);
 
+    println!("Shuffled deck Table:\n{}", table);
+
+    //teeeeests
     // Distribute two card to each players
     for seat in table.seats.iter_mut() {
         if let Some(card) = table.deck.cards.pop() {
@@ -438,5 +502,26 @@ fn main() {
         }
     }
 
-    println!("Table:\n{}", table);
+    for seat in table.seats.iter_mut() {
+        if let Some(card) = seat.hand.pop() {
+            online_board::play_card(&mut table.river, &seat.player, card);
+        }
+    }
+
+    online_board::store_winner_fold(&mut table, 1);
+
+    println!("first game Table:\n{}", table);
+
+    for seat in table.seats.iter_mut() {
+        if let Some(card) = seat.hand.pop() {
+            online_board::play_card(&mut table.river, &seat.player, card);
+        }
+    }
+
+    println!("mid second game Table:\n{}", table);
+
+    // Clear the table after the game
+    online_board::clear_table_after_game(&mut table);
+
+    println!("cleared Table:\n{}", table);
 }
